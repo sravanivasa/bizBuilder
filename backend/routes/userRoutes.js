9b587d150642e26110966fff762570eb50a45876
sravani <sravanivasa25@gmail.com>
2026-06-25 10:59:07 +0530

    updated changes in userRoute .js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
     console.log(req.body);
   try {
-    const user = await User.create(req.body);
-
+    const hashedPassword = await bcrypt.hash(
+      req.body.password,
+      10
+    );
+    
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    });
     res.status(201).json({
       success: true,
       data: user
