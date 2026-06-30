8db34dbc073202402a39c329d3f564c5bab23931
sravanivasa <sravani.vasa6@gmail.com>
2026-06-30 09:22:40 +0530

    adding Business model business.js

---
+++ backend/models/Business.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const businessSchema = new mongoose.Schema({
+
+    businessName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phoneNumber:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    website: {
+        type: String,
+        trim: true
+        
+    },
+    logo: {
+        type: String,
+        trim: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true 
+    }
+
+},
+    {
+        timestamps: true
+    }
+);
